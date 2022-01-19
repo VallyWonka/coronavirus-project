6fdf503e9df61197063e1afdf9a370a7bab698ff
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2022-01-19 11:39:51 +0300

    Update paths

--- preprocess.py
+++ preprocess.py
@@ -7,9 +7,9 @@ os.mkdir(f"{DATA_PATH}_preprocessed")
 
 
 for directory in os.listdir(DATA_PATH):
-    os.mkdir(f"{DATA_PATH}_preprocessed/{directory}_preprocessed")
+    os.mkdir(f"{DATA_PATH}_preprocessed/{directory}")
     for file in os.listdir(f"{DATA_PATH}/{directory}"):
         with open(f"{DATA_PATH}/{directory}/{file}", "r", encoding="utf-8") as fr:
             text = fr.read()
-            with open(f"{DATA_PATH}_preprocessed/{directory}_preprocessed/{file}", "w", encoding="utf-8") as fw:
+            with open(f"{DATA_PATH}_preprocessed/{directory}/{file}", "w", encoding="utf-8") as fw:
                 fw.write(preprocess(text))
